3089b8834465637c12f052921eb90bdb2a6f7322
Amr Samy <130178408+AmrSamy2@users.noreply.github.com>
2023-04-16 11:40:48 +0200

    added an end result and player score
    changed playround from several "if else"
    to "| |" and added a codition to add player score if player won and the same for computer.
    
    added an end result by comparing player score and computer score and the higher wins the match.

--- java.js
+++ java.js
@@ -1,4 +1,6 @@
-
+let playerscore = 0
+let compscore = 0
+let result = ""
 function getComputerChoice() {
   const computerAnswer = ['rock', 'paper', 'scissors']
   return computerAnswer[Math.floor(Math.random() * computerAnswer.length)];
@@ -12,21 +14,34 @@ function playround(playerSelection, computerSelection) {
   console.log(computerSelection)
   if (playerSelection == computerSelection) {
     return 'this is a tie!'
-  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-    return `you won! paper beats ${computerSelection}`
-  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-    return `you won! scissors beats ${computerSelection}`
-  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-    return `you won! rock beats ${computerSelection}`
-  } else { return 'you lost!' }
+  } else if (playerSelection == 'paper' && computerSelection == 'rock' ||
+    (playerSelection == 'scissors' && computerSelection == 'paper') ||
+    (playerSelection == 'rock' && computerSelection == 'scissors')) {
+    playerscore += 1;
+    return `you won! ${playerSelection} beats ${computerSelection}`
+  }
+
+  else {
+    compscore += 1
+    return 'you lost!'
+  }
+
 }
 
 const playerSelection = prompt('pick rock, paper or scissors!');
 computerSelection = getComputerChoice();
 
 function game() {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 10; i++) {
     console.log(playround(playerSelection, getComputerChoice()))
+    if (playerscore > compscore) {
+      result = 'you won!'
+    } else if (playerscore < compscore) {
+      result = 'you lost!'
+    }
   }
 }
 console.log(game());
+console.log('player:', playerscore);
+console.log('computer:', compscore);
+console.log (result)
